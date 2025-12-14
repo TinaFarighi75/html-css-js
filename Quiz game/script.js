e4c499bf075ba185a7473e1c7edab04e6b65e266
TinaFarighi75 <farighitina@gmail.com>
2025-12-14 14:22:20 +0330

    Defining and assigning values ​​to variables

--- Quiz game/script.js
+++ Quiz game/script.js
@@ -62,3 +62,9 @@ const quizQuestions = [
       ],
     },
   ];
+
+  let currentQuestionIndex = 0;
+  let score = 0;
+  let answersDisabled = false;
+  totalQuestionsSpan.textContent = quizQuestions.length;
+  maxScoreSpan.textContent = quizQuestions.length;  
